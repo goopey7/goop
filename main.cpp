59e15465649351ba1b61520f37cd1ec3e2b7b4c5
Sam Collier <samcollier916@gmail.com>
2023-09-21 17:44:22 +0100

    excalidraw

--- main.cpp
+++ main.cpp
@@ -12,28 +12,29 @@
 
 int main()
 {
-    glfwInit();
+	glfwInit();
 
-    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-    GLFWwindow* window = glfwCreateWindow(800, 600, "goop EDITOR", nullptr, nullptr);
+	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+	GLFWwindow* window = glfwCreateWindow(800, 600, "goop EDITOR", nullptr, nullptr);
 
-    uint32_t extensionCount = 0;
-    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+	uint32_t extensionCount = 0;
+	vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
 
-    std::cout << extensionCount << " extensions supported\n";
+	std::cout << extensionCount << " extensions supported\n";
 
-    glm::mat4 matrix;
-    glm::vec4 vec;
-    auto test = matrix * vec;
+	glm::mat4 matrix;
+	glm::vec4 vec;
+	auto test = matrix * vec;
 
 	say_hello();
-    while(!glfwWindowShouldClose(window)) {
-        glfwPollEvents();
-    }
+	while(!glfwWindowShouldClose(window))
+	{
+		glfwPollEvents();
+	}
 
-    glfwDestroyWindow(window);
+	glfwDestroyWindow(window);
 
-    glfwTerminate();
+	glfwTerminate();
 
-    return 0;
+	return 0;
 }
