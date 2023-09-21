60ee76e3ab43ae72e779c5e505d57785ef18f990
Sam Collier <samcollier916@gmail.com>
2023-09-21 17:08:53 +0100

    add vulkan + glfw

--- main.cpp
+++ main.cpp
@@ -1,12 +1,39 @@
-#include <thread>
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include <glm/vec4.hpp>
+#include <glm/mat4x4.hpp>
+
 #include <iostream>
+
 #include <rustlib.h>
 
 int main()
 {
-	std::cout << "Calling rust function...\n";
-	std::thread t(say_hello);
-	std::cout << "Doing cpp stuff..\n";
-	t.join();
-	return 0;
+    glfwInit();
+
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    GLFWwindow* window = glfwCreateWindow(800, 600, "goop EDITOR", nullptr, nullptr);
+
+    uint32_t extensionCount = 0;
+    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+
+    std::cout << extensionCount << " extensions supported\n";
+
+    glm::mat4 matrix;
+    glm::vec4 vec;
+    auto test = matrix * vec;
+
+	say_hello();
+    while(!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+
+    glfwTerminate();
+
+    return 0;
 }
