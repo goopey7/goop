8097b1525e66372994759562660de840bb32f430
Sam Collier <samcollier916@gmail.com>
2023-09-24 18:17:38 +0100

    select format, present mode, and extent

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
+#include <algorithm>
+#include <cstdint>
 #include <cstring>
+#include <limits>
 #include <optional>
 #include <set>
 #include <stdexcept>
 #include <vector>
-#include <vulkan/vulkan_core.h>
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 #include <cstdlib>
@@ -428,6 +430,62 @@ class HelloTriangleApp
 		}
 
 		return details;
+	}
+
+	VkSurfaceFormatKHR selectSurfaceFormat(const std::vector<VkSurfaceFormatKHR> &availableFormats)
+	{
+		for (const auto &availableFormat : availableFormats)
+		{
+			if (availableFormat.format == VK_FORMAT_B8G8R8A8_SRGB &&
+				availableFormat.colorSpace == VK_COLOR_SPACE_SRGB_NONLINEAR_KHR)
+			{
+				return availableFormat;
+			}
+		}
+
+		return availableFormats[0];
+	}
+
+	VkPresentModeKHR selectPresentMode(const std::vector<VkPresentModeKHR> &availablePresentModes)
+	{
+		for (const auto &availablePresentMode : availablePresentModes)
+		{
+			if (availablePresentMode == VK_PRESENT_MODE_MAILBOX_KHR)
+			{
+				return availablePresentMode;
+			}
+		}
+
+		return VK_PRESENT_MODE_FIFO_KHR;
+	}
+
+	VkExtent2D selectExtent(const VkSurfaceCapabilitiesKHR &capabilities)
+	{
+		// if extent.width is ui32 max then we are taking up max space in the window
+		// otherwise we must've specified a specific size
+		if (capabilities.currentExtent.width != std::numeric_limits<uint32_t>::max())
+		{
+			return capabilities.currentExtent;
+		}
+		else
+		{
+			int width, height;
+			glfwGetFramebufferSize(window, &width, &height);
+
+			VkExtent2D actualExtent = {
+				static_cast<uint32_t>(width),
+				static_cast<uint32_t>(height),
+			};
+
+			actualExtent.width = std::clamp(actualExtent.width, capabilities.minImageExtent.width,
+											capabilities.maxImageExtent.width);
+
+			actualExtent.height =
+				std::clamp(actualExtent.height, capabilities.minImageExtent.height,
+						   capabilities.maxImageExtent.height);
+
+			return actualExtent;
+		}
 	}
 
 	void mainLoop()
